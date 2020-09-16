79ed13298a17ec8d15c8bf83f855882e52db2854
Shriji <shrijike@gmail.com>
2020-09-16 23:38:16 +0530

    0.0.6
    Added tooltips and circle markers

---
+++ src/Leaflet.svelte
@@ -0,0 +1,175 @@
+<script>
+  import LoadSdk from "./LoadSdk.svelte";
+  import { createEventDispatcher } from "svelte";
+
+  let L = {};
+  let map = "";
+  const dispatch = createEventDispatcher();
+
+  export let options;
+
+  let {
+    zoom = 13,
+    maxZoom = 19,
+    minZoom = 1,
+    mapID = "map",
+    attributionControl = true,
+    center = [0, 0],
+    markers,
+    circles,
+    recenter = false,
+    scrollWheelZoom = true,
+    tileLayer = {
+      url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    },
+    controls = {
+      zoomControl: true,
+      position: "topleft",
+      scale: false,
+    },
+  } = options;
+
+  function initialise() {
+    setTimeout(() => {
+      L = window["L"];
+
+      createMap();
+      dispatch("ready");
+    }, 1);
+  }
+
+  function createMap() {
+    map = L.map(mapID, {
+      attributionControl,
+      zoomControl: controls.zoomControl,
+      minZoom,
+      maxZoom,
+    }).setView(center, zoom);
+    L.tileLayer(tileLayer.url, {
+      attribution: tileLayer.attribution,
+    }).addTo(map);
+
+    if (!scrollWheelZoom) {
+      map.scrollWheelZoom.disable();
+    }
+
+    let controlElement = L.control;
+    if (!controls.zoomControl) {
+      controlElement().remove();
+    }
+    if (controls.scale) {
+      controlElement.scale({ position: controls.position }).addTo(map);
+    }
+    if (controls.zoomControl && controls.position) {
+      map.removeControl(map.zoomControl);
+      controlElement
+        .zoom({
+          position: controls.position,
+        })
+        .addTo(map);
+    }
+
+
+    let icon;
+    let markersArray = [];
+    let circleArray = [];
+    let bounds;
+    let defaultIcon = {
+      iconUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png",
+      iconRetinaUrl:
+        "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png",
+      shadowUrl:
+        "https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png",
+      iconSize: [25, 41],
+      iconAnchor: [12, 41],
+      popupAnchor: [1, -34],
+      tooltipAnchor: [1, -34],
+      shadowSize: [41, 41],
+    };
+    if (markers) {
+      markers.map((e) => {
+        markersArray.push([e.lat, e.lng]);
+        if (e.icon) {
+          icon = L.icon(e.icon);
+        } else {
+          icon = L.icon(defaultIcon);
+        }
+        let marker = new L.marker(
+          [e.lat, e.lng],
+
+          { icon }
+        );
+        if (e.popup) {
+          makePopup(marker, e.popup);
+        }
+        if (e.tooltip) {
+          makeTooltip(marker, e.tooltip);
+        }
+        marker.addTo(map);
+      });
+    }
+    if (circles) {
+      circles.map((e) => {
+        circleArray.push([e.lat, e.lng]);
+           let circle = new L.circle(
+          [e.lat, e.lng],{ ...e }
+        );
+        if (e.popup) {
+          makePopup(circle, e.popup);
+        }
+        if (e.tooltip) {
+          makeTooltip(circle, e.tooltip);
+        }
+        circle.addTo(map);
+      });
+    }
+    function makeTooltip(marker, options) {
+      marker.bindTooltip(options.text, { ...options }).addTo(map);
+      if (options.isOpen) {
+        marker.openTooltip();
+      }
+    }
+
+    function makePopup(marker, options) {
+      marker
+        .bindPopup(options.text, {
+          closeOnClick: false,
+          autoClose: false,
+          ...options
+        })
+        .addTo(map);
+      if (options.isOpen) {
+        marker.openPopup();
+      }
+    }
+    if (recenter) {
+      if (markersArray.length == 1) {
+        map.panTo(L.latLng(markersArray[0][0], markersArray[0][1]));
+        map.setZoom(zoom);
+      } else {
+        bounds = new L.LatLngBounds(markersArray);
+        map.fitBounds(bounds);
+      }
+    }
+  }
+
+  function resizeMap() {
+    if (map) {
+      map.invalidateSize();
+    }
+  }
+</script>
+
+<style>
+  .map {
+    height: inherit;
+    width: inherit;
+  }
+</style>
+
+<svelte:window on:resize={resizeMap} />
+
+<div class="map" id={mapID} bind:this={mapID} />
+<LoadSdk on:ready={initialise} />
